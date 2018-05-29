421f6babe86063f41b9cbc0cbf3286e5ab3dd6b5
Natasha Calleia <nattycdb@gmail.com>
2018-05-29 19:43:58 -0300

    parse request path on server

--- uptime-monitor/index.js
+++ uptime-monitor/index.js
@@ -1,9 +1,21 @@
 const http = require('http')
+const { URL } = require('url')
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
+
+    // Get the URL and parse it
+    const parsedUrl = new URL(`localhost:${req.url}`)
+
+    // Get the path
+    const path = parsedUrl.pathname.replace(/^\/|\/+$/g, '')
+
+    // Send the response
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Hello, world!\n')
+
+    // Log the request path
+    console.log(`Request received on path: ${path}\n`)
 })
 
 // Start the server, and have it listen on port 3000
