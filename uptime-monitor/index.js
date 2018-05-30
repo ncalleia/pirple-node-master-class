9bdd51de93e9ddc5565762178e00dc6794eed91a
Natasha Calleia <nattycdb@gmail.com>
2018-05-30 13:29:12 -0300

    get query string object

--- uptime-monitor/index.js
+++ uptime-monitor/index.js
@@ -10,6 +10,13 @@ const server = http.createServer((req, res) => {
     // Get the path
     const path = parsedUrl.pathname.replace(/^\/|\/+$/g, '')
 
+    // Get query string as an object
+    const searchParams = parsedUrl.searchParams
+    let queryStringObject = {}
+    for (let pair of searchParams.entries()) {
+        queryStringObject[pair[0]] = pair[1]
+    }
+
     // Get the HTTP method
     const method = req.method.toUpperCase()
 
@@ -17,8 +24,10 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Hello, world!\n')
 
-    // Log the request path
-    console.log(`${method} request received on path: ${path}`)
+    // Log the request
+    const hasQuery = Object.keys(queryStringObject).length > 0
+    console.log(`${method} request received on path ${path}${hasQuery ?
+        `, with these query string parameters: ${JSON.stringify(queryStringObject)}` : ''}`)
 })
 
 // Start the server, and have it listen on port 3000
